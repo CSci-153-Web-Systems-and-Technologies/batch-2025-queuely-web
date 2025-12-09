e6680631b6d3009f69a2f08da6b6d00dc22dc39a
Milchzedek Cabales <cabalesmilchzedek@gmail.com>
2025-12-10 02:37:23 +0800

    Maded some changes

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -106,7 +106,7 @@ export default function UserProfilePage() {
 
     if (!user || history === null) { 
         return (
-            <div className="h-screen flex items-center justify-center">
+            <div className="h-screen flex items-center justify-center bg-[#E8F3E8]">
                 <Loader2 className="h-10 w-10 animate-spin text-[#1B4D3E]" />
             </div>
         );
